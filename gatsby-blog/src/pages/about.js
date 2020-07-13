7ae809b7ef7c9b114a296224ae9c89b938175422
Gerard Hynes <gkhynes@gmail.com>
2020-07-13 20:54:52 +0100

    Query and transform image for about page

--- gatsby-blog/src/pages/about.js
+++ gatsby-blog/src/pages/about.js
@@ -2,12 +2,14 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Img from "gatsby-image"
 
 export default function About({ data }) {
   return (
     <Layout>
       <SEO title="About" />
       <h1>An about page for a {data.site.siteMetadata.title}</h1>
+      <Img fluid={data.file.childImageSharp.fluid} alt="Jay Gatsby" />
       <p>
         If you want to learn about Gatsby, you can't do better than to read the{" "}
         <a href="https://www.gatsbyjs.org/docs/">official docs</a>.
@@ -21,6 +23,13 @@ export const query = graphql`
     site {
       siteMetadata {
         title
+      }
+    }
+    file(relativePath: { eq: "images/gatsby-about.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 800, maxHeight: 800, grayscale: true) {
+          ...GatsbyImageSharpFluid
+        }
       }
     }
   }
