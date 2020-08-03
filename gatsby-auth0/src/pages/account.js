2cff0efbb314324969648b19815c2b28a2d70c96
Gerard Hynes <gkhynes@gmail.com>
2020-08-03 21:23:52 +0100

    Add login

--- gatsby-auth0/src/pages/account.js
+++ gatsby-auth0/src/pages/account.js
@@ -1,16 +1,21 @@
 import React from "react"
 import { Router } from "@reach/router"
+import { login } from "../utils/auth"
 
 const Home = () => <p>Home</p>
 const Settings = () => <p>Settings</p>
 const Billing = () => <p>Billing</p>
 
-const Account = () => (
-  <Router>
-    <Settings path="/account/settings" />
-    <Billing path="/account/billing" />
-    <Home path="/account/" />
-  </Router>
-)
+const Account = () => {
+  login()
+
+  return (
+    <Router>
+      <Settings path="/account/settings" />
+      <Billing path="/account/billing" />
+      <Home path="/account/" />
+    </Router>
+  )
+}
 
 export default Account
